b151295b7f771fe666027ce4dd1b32d31238cf90
Andy Pearson <andy@londonparkour.com>
2020-05-25 16:39:08 +0100

    format

--- menu_posts_auto_add.php
+++ menu_posts_auto_add.php
@@ -17,15 +17,16 @@ Author URI: http://londonparkour.com
 //             --> post 4
 
 
-add_filter( 'wp_nav_menu_items', 'your_custom_menu_item', 10, 2 );
+add_filter('wp_nav_menu_items', 'your_custom_menu_item', 10, 2);
 
-//  ┌───────────────────────────────────────────┐ 
+//  ┌───────────────────────────────────────────┐
 //  │                                           │░
 //  │                Create Menu                │░
 //  │                                           │░
 //  └───────────────────────────────────────────┘░
 //   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-function your_custom_menu_item( $items, $args ) {
+function your_custom_menu_item($items, $args)
+{
 
     // Get the menu to add category posts to.
     $menu = wp_get_nav_menu_object($args->menu);
@@ -34,31 +35,32 @@ function your_custom_menu_item( $items, $args ) {
     $category_id = get_field('category', $menu);
 
     // If there isn't a linked category, abort.
-    if ($category_id == null || $category_id == false || $category_id == ''){ return $items; }
+    if ($category_id == null || $category_id == false || $category_id == '') {
+        return $items;
+    }
 
-    // get all child categories of parent 
+    // get all child categories of parent
     $child_categories = get_categories(
         array( 'parent' => $category_id )
     );
 
     // Loop through each child category
-    foreach( $child_categories as $count => $child_category ) {
-    
+    foreach ($child_categories as $count => $child_category) {
         $items .= build_child_category_menu($child_category, $args, $menu, $count);
-
     }
 
     return $items;
 }
 
 
-//  ┌───────────────────────────────────────────┐ 
+//  ┌───────────────────────────────────────────┐
 //  │                                           │░
 //  │      Build menu from child category       │░
 //  │                                           │░
 //  └───────────────────────────────────────────┘░
 //   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-function build_child_category_menu($child_category, $args, $menu, $count){
+function build_child_category_menu($child_category, $args, $menu, $count)
+{
 
     // Open the child category to menu.
     $current_item = '<li class="menu-item menu-item-level-1 menu-item-'.$count.' menu-item-'. $child_category->slug .'">';
@@ -66,33 +68,30 @@ function build_child_category_menu($child_category, $args, $menu, $count){
     // Add the child category name.
     $current_item .= $child_category->cat_name ;
 
-        // get list of posts from child category.
-        $argument_array = array( 
+    // get list of posts from child category.
+    $argument_array = array(
             'numberposts'       => 5000,
-            'category'          => $child_category->cat_ID, 
+            'category'          => $child_category->cat_ID,
             'post_type'         =>  'post',
 
             'order'     => 'ASC',
             'orderby'   => 'order_clause',
             'meta_query' => array(
-                 'order_clause' => array(
-                      'key' => 'menu_position',
+                'order_clause' => array(
+                    'key' => 'menu_position',
                       'type' => 'NUMERIC' // unless the field is not a number
-                 )
+                )
             )
         );
 
-       
+    $postslist = get_posts($argument_array);
 
-        $postslist = get_posts( $argument_array );   
-
-        // Check we are using the right menu
-        if (is_single() && $args->menu->name == $menu->name) {
+    // Check we are using the right menu
+    if (is_single() && $args->menu->name == $menu->name) {
 
             // Build up a list of posts within child category.
-            $current_item .= build_list_of_items($postslist);
-
-        }
+        $current_item .= build_list_of_items($postslist);
+    }
 
     // Close the current child category.
     $current_item .= '</li>';
@@ -103,13 +102,14 @@ function build_child_category_menu($child_category, $args, $menu, $count){
 
 
 
-//  ┌───────────────────────────────────────────┐ 
+//  ┌───────────────────────────────────────────┐
 //  │                                           │░
 //  │           Create list of items            │░
 //  │                                           │░
 //  └───────────────────────────────────────────┘░
 //   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-function build_list_of_items($list_of_posts){
+function build_list_of_items($list_of_posts)
+{
 
     // Open a new list.
     $list = '<ul class="sub-menu">';
@@ -119,7 +119,6 @@ function build_list_of_items($list_of_posts){
 
         // Build the link
         $list .= build_link($post_object, $count);
-
     }
 
     // Close list
@@ -132,34 +131,37 @@ function build_list_of_items($list_of_posts){
 
 
 
-//  ┌───────────────────────────────────────────┐ 
+//  ┌───────────────────────────────────────────┐
 //  │                                           │░
 //  │            Create item in list            │░
 //  │                                           │░
 //  └───────────────────────────────────────────┘░
 //   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-function build_link($post_object, $count){
+function build_link($post_object, $count)
+{
 
     // Get Icon from post.
     $icon = get_field('material_icon_code', $post_object->ID);
     $icon_string = '';
-    if ($icon != ''){
+    if ($icon != '') {
         $icon_string = '<i class="material-icons menu-item-icon">'.$icon.'</i>';
     }
 
     // Check if link is current page.
     $current_class = '';
-    $current_post = str_replace('/','', $_SERVER['REQUEST_URI']);
-    if ($post_object->post_name == $current_post){ $current_class = ' menu-item-current '; }
+    $current_post = str_replace('/', '', $_SERVER['REQUEST_URI']);
+    if ($post_object->post_name == $current_post) {
+        $current_class = ' menu-item-current ';
+    }
 
-    $relative_url = preg_replace( '/.*\.com/' ,'', $post_object->guid );
+    $relative_url = preg_replace('/.*\.com/', '', $post_object->guid);
 
     // output.
     $link = '<li class="menu-item menu-item-level-2 menu-item-'.$count.' menu-item-'. $post_object->ID .' menu-item-'. $post_object->post_name . $current_class . '">';
-        $link .= '<a class="menu-item-link '. $current_class . '" href="' . $relative_url .  '">';
-            $link .= $icon_string;
-            $link .= $post_object->post_title;
-        $link .= '</a>';
+    $link .= '<a class="menu-item-link '. $current_class . '" href="' . $relative_url .  '">';
+    $link .= $icon_string;
+    $link .= $post_object->post_title;
+    $link .= '</a>';
     $link .= '</li>';
 
     return $link;
